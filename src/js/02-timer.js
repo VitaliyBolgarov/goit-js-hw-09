b463275cc86d61365dea8ce12ae84621575e1445
VitaliyBolgarov <125477680+VitaliyBolgarov@users.noreply.github.com>
2023-07-09 21:55:24 +0300

    add 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,110 +1,69 @@
-import flatpickr from 'flatpickr';
-import 'flatpickr/dist/flatpickr.min.css';
-import { Report } from 'notiflix/build/notiflix-report-aio';
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix'
 
-const TIMER_DELAY = 1000;
-let intervalId = null;
-let selectedDate = null;
-let currentDate = null;
+const buttonStartEl = document.querySelector('button[data-start]');
+const daysEl = document.querySelector('span[data-days]');
+const hoursEl = document.querySelector('span[data-hours]');
+const minutesEl = document.querySelector('span[data-minutes]');
+const secondsEl = document.querySelector('span[data-seconds]');
 
-const refs = {
-  dateInput: document.querySelector('input#datetime-picker'),
-  btnStartTimer: document.querySelector('button[data-start-timer]'),
-  daysRemaining: document.querySelector('[data-days]'),
-  hoursRemaining: document.querySelector('[data-hours]'),
-  minutesRemaining: document.querySelector('[data-minutes]'),
-  secondsRemaining: document.querySelector('[data-seconds]'),
-};
+buttonStartEl.classList.add('button--inactive');
+buttonStartEl.addEventListener('click', startTimer);
 
-refs.btnStartTimer.disabled = true;
-refs.btnStartTimer.addEventListener('click', timerStart);
+const currentDate = new Date();
+let dateFromUsrer;
 
-let remainingTime = 0;
-
-const options = {
+flatpickr('#datetime-picker', {
   enableTime: true,
   time_24hr: true,
-  defaultDate: new Date(),
+  defaultDate: currentDate,
   minuteIncrement: 1,
   onClose(selectedDates) {
-    onDateCheck(selectedDates);
-  },
-};
-
-flatpickr(refs.dateInput, options);
-
-Report.info(
-  '👋 Greeting, my Friend!',
-  'Please, choose a date and click on start',
-  'Okay'
-);
-
-function onDateCheck(selectedDates) {
-  selectedDate = selectedDates[0].getTime();
-  currentDate = new Date().getTime();
-
-  if (selectedDate > currentDate) {
-    refs.btnStartTimer.disabled = false;
-    Report.success(
-      '🥰 Congratulation! Click on start!',
-      '"Do not try to become a person of success but try to become a person of value." <br/><br/>- Albert Einstein',
-      'Okay'
-    );
-    return;
-  }
-  Report.failure(
-    '🥺 Ooops...',
-    'Please, choose a date in the future and remember: "Knowledge rests not upon truth alone, but upon error also." - Carl Gustav Jung',
-    'Okay'
-  );
-}
-
-function timerStart() {
-  intervalId = setInterval(() => {
-    currentDate = new Date().getTime();
-    if (selectedDate - currentDate <= 1000) {
-      clearInterval(intervalId);
-      refs.btnStartTimer.disabled = true;
-      refs.dateInput.disabled = false;
-      Report.info(
-        '👏 Congratulation! Timer stopped!',
-        'Please, if you want to start timer, choose a date and click on start or reload this page',
-        'Okay'
-      );
+    dateFromUsrer = selectedDates[0];
+    if (currentDate > dateFromUsrer) {
+      Notiflix.Notify.failure('Please choose a date in the future');
       return;
+    } else
+      buttonStartEl.classList.remove('button--inactive');
+  },
+});
+
+function startTimer() {
+  const id = setInterval(() => {
+    const current = new Date();
+    const timerTime = convertMs(dateFromUsrer - current);
+    if (timerTime.seconds >= 0) {
+      drawingTextContent(timerTime)
     } else {
-      refs.btnStartTimer.disabled = true;
-      refs.dateInput.disabled = true;
-      currentDate += 1000;
-      remainingTime = Math.floor(selectedDate - currentDate);
-      convertMs(remainingTime);
+      clearInterval(id);
+      Notiflix.Notify.success('Congratulation! Time is ower.');
     }
-  }, TIMER_DELAY);
+  }, 1000)
 }
 
-function createMarkup({ days, hours, minutes, seconds }) {
-  refs.daysRemaining.textContent = days;
-  refs.hoursRemaining.textContent = hours;
-  refs.minutesRemaining.textContent = minutes;
-  refs.secondsRemaining.textContent = seconds;
-}
-
-function addLeadingZero(value) {
-  return String(value).padStart(2, '0');
+function drawingTextContent(timerTime) {
+  daysEl.textContent = timerTime.days.toString().padStart(2, '0');
+  hoursEl.textContent = timerTime.hours.toString().padStart(2, '0');
+  minutesEl.textContent = timerTime.minutes.toString().padStart(2, '0');
+  secondsEl.textContent = timerTime.seconds.toString().padStart(2, '0');
 }
 
 function convertMs(ms) {
+  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  const days = addLeadingZero(Math.floor(ms / day));
-  const hours = addLeadingZero(Math.floor((ms % day) / hour));
-  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-  const seconds = addLeadingZero(
-    Math.floor((((ms % day) % hour) % minute) / second)
-  );
-  createMarkup({ days, hours, minutes, seconds });
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
   return { days, hours, minutes, seconds };
 }
